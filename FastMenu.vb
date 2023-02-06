ec0f074df046d264f8e72c44facbe8a2cdea5b40
Rixolino <71252421+Rixolino@users.noreply.github.com>
2023-02-06 21:13:19 +0100

    Add files via upload

---
+++ FastMenu.vb
@@ -0,0 +1,222 @@
+﻿Public Class FastMenu
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Math_Graphics.Close()
+        Normal.Show()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Show()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Show()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Show()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Show()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Show()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Show()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Math_Graphics.Close()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Show()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        Math_Graphics.Close()
+        BinaryOp.Show()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+
+    Private Sub FastMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        Me.TopMost = True
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Me.TopMost = False
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Math_Graphics.Show()
+        BinaryOp.Close()
+        DEmore.Close()
+        Formulas.Close()
+        fstequ.Close()
+        FunctionConverter.Close()
+        Geometry.Close()
+        Scientific.Close()
+        Inequalities.Close()
+        Linearequations.Close()
+        LinEquationTriple.Close()
+        Programmer.Close()
+        rInequalities.Close()
+        SimpleAlgebra.Close()
+        sndequ.Close()
+        TEmore.Close()
+        Normal.Hide()
+    End Sub
+End Class
